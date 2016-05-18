1d9dfb734309c5c6e884106cbe8cd642e149e5b1
Abhishek <abhicoders@gmail.com>
2016-05-19 00:04:21 +0530

    Initial Analysis

---
+++ Bio-Response/src/Bio-Response-EDA.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+# load in train and test files
+train = pd.read_csv('../data/train.csv')
+test = pd.read_csv('../data/test.csv'
---
+++ Give-Me-Credit/src/Give-Me-Credit-EDA.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# load train and test set
+train = pd.read_csv('../data/cs-training.csv')
+test = pd.read_csv('../data/cs-test.csv')
